1d73c3eeba976c801b72013bdabb687f170a8273
Thomas Müller <mimmi20@live.de>
2023-12-28 10:37:03 +0100

    fix issue

--- src/FormDateTime.php
+++ src/FormDateTime.php
@@ -14,7 +14,7 @@ namespace Mimmi20\LaminasView\BootstrapForm;
 
 use Laminas\Form\ElementInterface;
 
-/** @deprecated This element is deprecated as it has been removed from WHATWG HTML, use {@see \Mimmi20\LaminasView\BootstrapForm\FormDateTimeLocal} instead */
+/** @deprecated This element is deprecated as it has been removed from WHATWG HTML, use {@see FormDateTimeLocal} instead */
 final class FormDateTime extends FormInput
 {
     /**
